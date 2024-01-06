92fca58cdfef5b7ba70f5002eadfb416c51b5065
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-01-06 19:16:08 +0800

    sh

--- scripts/360V6/360V6-cj.sh
+++ scripts/360V6/360V6-cj.sh
@@ -17,7 +17,7 @@
 echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
 
 # '修改默认主机名'
-sed -i 's/Portable/OpenWrt 360V6/g' package/addition/default-settings/files/99-default-settings
+sed -i 's/Portable/360V6/g' package/extra/default-settings/files/99-default-settings
 
 #　添加新插件
 
@@ -28,12 +28,13 @@ sed -i 's/Portable/OpenWrt 360V6/g' package/addition/default-settings/files/99-d
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 #删除不用插件
-# rm -rf package/feeds/luci/luci-app-ssr-plus
+
+rm -rf package/feeds/luci/luci-app-ssr-plus
+
 
 # 修改插件名字
 sed -i 's/"挂载点"/"磁盘挂载"/g' `grep "挂载点" -rl ./`
 # sed -i 's/"Argonne 主题设置"/"主题设置"/g' `grep "Argonne 主题设置" -rl ./`
-sed -i 's/"ShadowSocksR Plus+"/"你好世界"/g' `grep "ShadowSocksR Plus+" -rl ./`
 sed -i 's/"解锁网易云灰色歌曲"/"音乐解锁"/g' `grep "解锁网易云灰色歌曲" -rl ./`
 sed -i 's/"状态"/"系统状态"/g' `grep "状态" -rl ./`
 sed -i 's/"接口"/"有线设置"/g' `grep "接口" -rl ./`
@@ -53,21 +54,19 @@ sed -i 's/"网络"/"网络设置"/g' `grep "网络" -rl ./`
 sed -i 's/"MWAN3 分流助手"/"分流助手"/g' `grep "MWAN3 分流助手" -rl ./`
 
 # 修改默认IP
-sed -i 's/192.168.10.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.10.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 #　web登陆密码从password修改为空
-# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/addition/default-settings/files/99-default-settings
+# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' package/extra/default-settings/files/99-default-settings
 
 #　固件版本号添加个人标识和日期
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/addition/default-settings/files/99-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/extra/default-settings/files/99-default-settings
 
 #　编译的固件文件名添加日期
-sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=360V6ssr-$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=360V6cj-$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
 #　修改主机名
 sed -i "s/hostname='OpenWrt'/hostname='QihooV6'/g" package/base-files/files/bin/config_generate
 
 # 添加自动挂载磁盘脚本
 # mkdir -p files/etc/hotplug.d/block && wget -O files/etc/hotplug.d/block/30-usbmount https://raw.githubusercontent.com/fichenx/P3TERX_Actions-OpenWrt/main/files/etc/hotplug.d/block/30-usbmount && chmod 755 files/etc/hotplug.d/block/30-usbmount
-
-
--- scripts/360V6/360V6-zy.sh
+++ scripts/360V6/360V6-zy.sh
@@ -63,10 +63,12 @@ sed -i 's/192.168.10.1/192.168.3.1/g' package/base-files/files/bin/config_genera
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='FICHEN(\$\(TZ=UTC-8 date +%Y-%m-%d\))@OpenWrt '/g" package/extra/default-settings/files/99-default-settings
 
 #　编译的固件文件名添加日期
-sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=360V6chunjinban-$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=360V6zy-$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
 #　修改主机名
 sed -i "s/hostname='OpenWrt'/hostname='QihooV6'/g" package/base-files/files/bin/config_generate
 
 # 添加自动挂载磁盘脚本
 # mkdir -p files/etc/hotplug.d/block && wget -O files/etc/hotplug.d/block/30-usbmount https://raw.githubusercontent.com/fichenx/P3TERX_Actions-OpenWrt/main/files/etc/hotplug.d/block/30-usbmount && chmod 755 files/etc/hotplug.d/block/30-usbmount
+
+
